a5ea192b2c9fe425f058b4073916468b44976401
JackRedds <Redep028@umn.edu>
2026-03-04 15:06:25 -0600

    forgot to update init

--- ml4gw/waveforms/adhoc/__init__.py
+++ ml4gw/waveforms/adhoc/__init__.py
@@ -1,6 +1,6 @@
 from .ringdown import Ringdown
 from .sine_gaussian import SineGaussian
 from .white_noise_burst import WhiteNoiseBurst
-from .cosmic_string import GenerateString
+from .cosmic_string import CosmicString
 from .sine_gaussian import MultiSineGaussian
 from .gaussian import Gaussian
